95b76109e173bd399dd3f501958e1ad13446f6cb
GalaxyIgor <igornbelisario@gmail.com>
2025-10-25 19:19:33 -0300

    fix

--- Livraria.sql
+++ Livraria.sql
@@ -9,6 +9,16 @@ CREATE TABLE Autor(
     PRIMARY KEY (idAutor)
 );
 
+CREATE TABLE Editora(
+	idEditora INT NOT NULL,
+    nomeEditora VARCHAR(100) NOT NULL,
+    cnpjEditora VARCHAR(20) NOT NULL UNIQUE,
+    LocalEditora VARCHAR(45) NOT NULL,
+    
+    PRIMARY KEY (idEditora)
+    
+);
+
 CREATE TABLE EstoqueLivros(
 	idEstoqueLivros INT NOT NULL,
     quantidade INT NOT NULL,
@@ -16,6 +26,23 @@ CREATE TABLE EstoqueLivros(
     UltimaMovimentacao VARCHAR(45) NOT NULL,
     
     PRIMARY KEY (idEstoqueLivros)
+);
+
+CREATE TABLE Livro(
+	idLivro INT NOT NULL,
+    títuloLivro VARCHAR(150) NOT NULL,
+    DataPublicacaoLivro DATE NOT NULL,
+    precoLivro DECIMAL(10,2) NOT NULL,
+    estoqueLivro INT NOT NULL,
+    PaginasLivro INT NOT NULL,
+    Editora_idEditora INT NOT NULL,
+    Autor_idAutor INT NOT NULL,
+    EstoqueLivros_idEstoqueLivros INT NOT NULL,
+    
+    PRIMARY KEY (idLivro),
+	FOREIGN KEY (Editora_idEditora) REFERENCES Editora(idEditora),
+	FOREIGN KEY (Autor_idAutor) REFERENCES Autor(idAutor),
+	FOREIGN KEY (EstoqueLivros_idEstoqueLivros) REFERENCES EstoqueLivros(idEstoqueLivros)
 );
 
 CREATE TABLE DetalhesDoLivro(
@@ -32,42 +59,15 @@ CREATE TABLE DetalhesDoLivro(
     
 );
 
-CREATE TABLE Livro(
-	idLivro INT NOT NULL,
-    títuloLivro VARCHAR(150) NOT NULL,
-    DataPublicacaoLivro DATE NOT NULL,
-    precoLivro DECIMAL(10,2) NOT NULL,
-    estoqueLivro INT NOT NULL,
-    PaginasLivro INT NOT NULL,
-    Editora_idEditora INT NOT NULL,
-    Autor_idAutor INT NOT NULL,
-    EstoqueLivros_idEstoqueLivros INT NOT NULL,
+CREATE TABLE Cliente(
+	idCliente INT NOT NULL,
+    nomeCliente VARCHAR(120) NOT NULL,
+    emailCliente VARCHAR(120) NOT NULL UNIQUE,
+    telefoneCliente VARCHAR(45) NOT NULL UNIQUE,
     
-    PRIMARY KEY (idLivro, Autor_idAutor,EstoqueLivros_idEstoqueLivros)
+    PRIMARY KEY (idCliente)
     
 );
-
-CREATE TABLE Editora(
-	idEditora INT NOT NULL,
-    nomeEditora VARCHAR(100) NOT NULL,
-    cnpjEditora VARCHAR(20) NOT NULL UNIQUE,
-    LocalEditora VARCHAR(45) NOT NULL,
-    
-    PRIMARY KEY (idEditora)
-    
-);
-
-
-CREATE TABLE Pedido_has_Livro(
-	Pedido_idPedido INT NOT NULL,
-    Livro_idLivro INT NOT NULL,
-    Livro_Editora_idEditora INT NOT NULL,
-    quantidade INT NOT NULL,
-    
-    PRIMARY KEY (Pedido_idPedido, Livro_idLivro,Livro_Editora_idEditora)
-    
-);
-
 
 CREATE TABLE Pedido(
 	idPedido INT NOT NULL,
@@ -77,6 +77,7 @@ CREATE TABLE Pedido(
     PRIMARY KEY (idPedido)
     
 );
+
 CREATE TABLE Cliente_has_Pedido(
 	Cliente_idCliente INT NOT NULL,
     Pedido_idPedido INT NOT NULL,
@@ -84,12 +85,13 @@ CREATE TABLE Cliente_has_Pedido(
     PRIMARY KEY (Cliente_idCliente, Pedido_idPedido)
     
 );
-CREATE TABLE Cliente(
-	idCliente INT NOT NULL,
-    nomeCliente VARCHAR(120) NOT NULL,
-    emailCliente VARCHAR(120) NOT NULL UNIQUE,
-    telefoneCliente VARCHAR(45) NOT NULL UNIQUE,
+
+CREATE TABLE Pedido_has_Livro(
+	Pedido_idPedido INT NOT NULL,
+    Livro_idLivro INT NOT NULL,
+    Livro_Editora_idEditora INT NOT NULL,
+    quantidade INT NOT NULL,
     
-    PRIMARY KEY (idCliente)
+    PRIMARY KEY (Pedido_idPedido, Livro_idLivro,Livro_Editora_idEditora)
     
 );

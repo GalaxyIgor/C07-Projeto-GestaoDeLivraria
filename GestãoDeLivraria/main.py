63f8a02e01f536abe69ec4d25e857a80c54764b8
GalaxyIgor <igornbelisario@gmail.com>
2025-11-13 20:25:34 -0300

    feat: possivel menu

--- GestãoDeLivraria/main.py
+++ GestãoDeLivraria/main.py
@@ -1,1 +1,28 @@
 #main
+
+print("=============Seja Bem Vindo Ao Gestor De Livraria!=============")
+
+print("=========================Menu Inicial=========================")
+print("Digite o numero da opção que melhor se encaixe no desejado!")
+print("1 - Listar livros do estoque e suas quantidades")
+print("1 - Resultado")
+print("Vc deseja fazer alguma alteração?(y/n)")
+print("Qual tipo de alteraço?")
+print("1 - Quero alterar um livro já existente no sistema")
+print("1.y.1 - Resultado Final")
+print("2 - Quero deletar um livro existente no sistema")
+print("1.y.3 - Resultado Final")
+print("3 - Quero adicionar um livro no sistema")
+print("1.y.3 - Resultado Final")
+print("Vc deseja fazer mais alguma alteração?(y/n)")
+
+print("2 - Buscar um livro no sistema")
+print("2 - Resultado da Busca(achou ou não)")
+print("Gostaria de ver as informações do livro?(y/n)")
+print("2.y - Resultado Final")
+print("Vc deseja fazer mais uma busca?(y/n)")
+
+print("3 - Registrar uma venda")
+print("Digite as infos do cliente:")
+print("Digite as infos do pedido:")
+print("Resultado final Extrato")
